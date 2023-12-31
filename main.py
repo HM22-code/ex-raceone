e7545db774f785c45048953085268cbc3aeaf9cb
HM22-code <H.M-22@outlook.fr>
2023-12-31 17:06:15 +0100

    Fix indent

--- main.py
+++ main.py
@@ -5,48 +5,29 @@ from objects.background import Background
 from objects.floor import Floor
 
 def main():
-    
-    #init pygame
     pygame.init()
-    
     screen = pygame.display.set_mode((configs.SCREEN_WIDTH, configs.SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-
-    '''
-    pygame.display.set_icon(icon)
-    pygame.display.set_caption(title)
-    '''
-    
-    #run loop
     running = True
-    
     assets.load_sprites()
-    
     sprites = pygame.sprite.LayeredUpdates()
-    
     Background(0, sprites)
     Background(1, sprites)
-    
     Floor(0, sprites)
     Floor(1, sprites)
     
     while running:
-
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 running = False
                 break
-    
+
         screen.fill("green")
-        
         sprites.draw(screen)
         sprites.update()
-        
         pygame.display.flip()
         clock.tick(configs.FPS)
-    
+        
     pygame.quit()
     quit()
 
