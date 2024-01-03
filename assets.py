40f85a803b2fd16b4884dd32e6a673f8fa4ad1c7
HM22-code <H.M-22@outlook.fr>
2024-01-03 21:21:59 +0100

    Adding loading audios

--- assets.py
+++ assets.py
@@ -2,6 +2,7 @@ import os
 import pygame
 
 sprites = {}
+audios = {}
 
 def load_sprites():
     path = os.path.join(os.path.abspath(os.curdir), "assets", "sprites")
@@ -10,3 +11,11 @@ def load_sprites():
         
 def get_sprite(name):
     return sprites[name]
+
+def load_audios():
+    path = os.path.join(os.path.abspath(os.curdir), "assets", "audios")
+    for file in os.listdir(path):
+        audios[file.split('.')[0]] = pygame.mixer.Sound(os.path.join(path, file))
+        
+def get_audio(name):
+    return audios[name]
