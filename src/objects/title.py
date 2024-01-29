c611345b9fabc9d14aaef25e2ceda459e35f6bbb
HM22-code <H.M-22@outlook.fr>
2024-01-29 13:36:11 +0100

    Adding scale property

--- src/objects/title.py
+++ src/objects/title.py
@@ -10,8 +10,11 @@ class Title(pygame.sprite.Sprite):
         pygame (_type_): sprite
     """
     
+    SCALE = 3
+    
     def __init__(self, *groups):
         super().__init__(*groups)
         self._layer = Layers.UI
         self.image = assets.get_sprite("title")
+        self.image = pygame.transform.scale(self.image, (int(self.image.get_width() * self.SCALE), int(self.image.get_height() * self.SCALE)))
         self.rect = self.image.get_rect(center=(configs.SCREEN_WIDTH / 2, configs.SCREEN_HEIGHT / 6))
