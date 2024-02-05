9aa0f955df6f6e4d4ce0f56ed63206a0102b2a30
HM22-code <H.M-22@outlook.fr>
2024-02-05 16:05:24 +0100

    Adding option state

---
+++ src/states/option.py
@@ -0,0 +1,29 @@
+import pygame
+import assets
+from classes.state import State
+from objects.background import Background
+
+
+class Option(State):
+    """ Option state class
+
+    Args:
+        State (_type_): state
+    """
+    
+    def __init__(self, game):
+        super().__init__(game)
+        # Sprite Groups
+        self.sprites = pygame.sprite.LayeredUpdates()
+        # Create Game objects
+        Background(self.sprites)
+        
+    def run(self):
+        # Draw
+        self.sprites.draw(self.game.screen)
+        # Update
+        self.sprites.update()
+        
+    def handle_event(self, event):
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            self.game.set_state(self.game.get_previous_state())
