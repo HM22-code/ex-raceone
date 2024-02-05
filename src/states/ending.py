19b671699e7afa7bf21afa93ddadfe898e10e937
HM22-code <H.M-22@outlook.fr>
2024-02-05 16:05:13 +0100

    Adding ending state

---
+++ src/states/ending.py
@@ -0,0 +1,38 @@
+import pygame
+import assets
+from classes.state import State
+from objects.background import Background
+
+
+class Ending(State):
+    """ Ending state class
+
+    Args:
+        State (_type_): state
+    """
+    
+    def __init__(self, game):
+        super().__init__(game)
+        # Sprite Groups
+        self.sprites = pygame.sprite.LayeredUpdates()
+        # Create Game objects
+        Background(self.sprites)
+        # Background music
+        self.music = assets.get_audio("ending")
+        self.music.set_volume(0.3)
+        
+    def run(self):
+        # Draw
+        self.sprites.draw(self.game.screen)
+        # Update
+        self.sprites.update()
+        
+    def handle_event(self, event):
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            self.game.set_state(self.game.get_previous_state())
+        
+    def enter_state(self):
+        self.music.play(loops = -1)
+    
+    def exit_state(self):
+        self.music.stop()
