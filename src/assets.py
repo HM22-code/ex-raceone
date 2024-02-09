37cf52a013650b7812baed67680cb80e8f79d179
HM22-code <H.M-22@outlook.fr>
2024-02-09 10:37:05 +0100

    Adding font assets loading

--- src/assets.py
+++ src/assets.py
@@ -3,6 +3,7 @@ import pygame
 
 sprites = {}
 audios = {}
+fonts = {}
 
 def load_sprites():
     """ Load all sprites from assets/images/
@@ -10,6 +11,18 @@ def load_sprites():
     path = os.path.join(os.path.abspath(os.curdir), "assets", "images")
     for file in os.listdir(path):
         sprites[file.split('.')[0]] = pygame.image.load(os.path.join(path, file)).convert_alpha()
+
+def load_sprite(file):
+    """ Load a sprite from assets/images/ from file
+
+    Args:
+        file (_type_): file
+
+    Returns:
+        _type_: image
+    """
+    path = os.path.join(os.path.abspath(os.curdir), "assets", "images")
+    return pygame.image.load(os.path.join(path, file)).convert_alpha()
         
 def get_sprite(name):
     """ Get sprite from name
@@ -29,6 +42,18 @@ def load_audios():
     for file in os.listdir(path):
         audios[file.split('.')[0]] = pygame.mixer.Sound(os.path.join(path, file))
         
+def load_audio(file):
+    """ Load an audio from assets/audios/ from file
+
+    Args:
+        file (_type_): file
+
+    Returns:
+        _type_: audio
+    """
+    path = os.path.join(os.path.abspath(os.curdir), "assets", "audios")
+    return pygame.mixer.Sound(os.path.join(path, file))
+        
 def get_audio(name):
     """ Get audio from name
 
@@ -39,3 +64,28 @@ def get_audio(name):
         _type_: audio
     """
     return audios[name]
+
+def load_fonts():
+    """ Load all fonts from assets/fonts/ with default font size
+    """
+    path = os.path.join(os.path.abspath(os.curdir), "assets", "fonts")
+    for file in os.listdir(path):
+        fonts[file.split('.')[0]] = pygame.font.Font(os.path.join(path, file), 36)
+        
+def load_font(file, size):
+    """ Load a font from assets/fonts/ from file
+
+    Args:
+        file (_type_): file
+        size (_type_): int
+
+    Returns:
+        _type_: font
+    """
+    path = os.path.join(os.path.abspath(os.curdir), "assets", "fonts")
+    return pygame.font.Font(os.path.join(path, file), size)
+    
+        
+def get_font(name):
+    return fonts[name]
+    
--- src/objects/text.py
+++ src/objects/text.py
@@ -1,5 +1,6 @@
 import pygame.sprite
 import configs
+import assets
 from layers import Layers
 
 class Text(pygame.sprite.Sprite):
@@ -12,6 +13,6 @@ class Text(pygame.sprite.Sprite):
     def __init__(self, text, *groups):
         super().__init__(*groups)
         self._layer = Layers.UI
-        self.font = pygame.font.SysFont(None, 36)
+        self.font = assets.load_font("16-bit-font.ttf", 36)
         self.image = self.font.render(text, True, (255, 255, 255))
         self.rect = self.image.get_rect(center=(self.image.get_rect().center))
