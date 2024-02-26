b3ffce9bf62311ba498ddbcd389f0a731dfeb1e5
HM22-code <H.M-22@outlook.fr>
2024-02-26 14:03:53 +0100

    Fix title position

--- src/objects/title.py
+++ src/objects/title.py
@@ -17,4 +17,4 @@ class Title(pygame.sprite.Sprite):
         self._layer = Layers.UI
         self.image = utils.assets.get_sprite("title")
         self.image = pygame.transform.scale(self.image, (int(self.image.get_width() * self.SCALE), int(self.image.get_height() * self.SCALE)))
-        self.rect = self.image.get_rect(center=(configs.SCREEN_WIDTH / 2, configs.SCREEN_HEIGHT / 6))
+        self.rect = self.image.get_rect(center=(configs.SCREEN_WIDTH // 2, configs.SCREEN_HEIGHT // 6))
