bbe7259f9fa950d0a64aa5151be2e83d3e3eecbd
HM22-code <H.M-22@outlook.fr>
2024-03-07 15:12:50 +0100

    Adding player animation

--- src/objects/player.py
+++ src/objects/player.py
@@ -16,11 +16,15 @@ class Player(pygame.sprite.Sprite):
         self._layer = Layers.PLAYER
         self.bullets = bullets
         self.sprites = sprites
-        self.image = utils.assets.get_sprite("player-1.png")
+        self.image_idle = utils.assets.get_sprite("player-1.png")
+        self.image_down = utils.assets.get_sprite("player-2.png")
+        self.image_up = utils.assets.get_sprite("player-3.png")
+        self.image = self.image_idle
         self.rect = self.image.get_rect(topleft=(50, configs.SCREEN_HEIGHT//2))
         self.velocity = 2
         
     def update(self, dt):
+        self.image = self.image_idle
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and self.rect.x > 0:
             self.rect.x -= self.velocity
@@ -28,8 +32,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += self.velocity
         if keys[pygame.K_UP] and self.rect.y > 0:
             self.rect.y -= self.velocity
+            self.image = self.image_up
         if keys[pygame.K_DOWN] and self.rect.y < configs.SCREEN_HEIGHT - self.rect.height:
             self.rect.y += self.velocity
+            self.image = self.image_down
                 
     def shoot(self):
         bullet = Bullet(self.rect.right, self.rect.centery)
