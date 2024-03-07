36e423cb23762091e4a1515b6cbb5a257099ac55
HM22-code <H.M-22@outlook.fr>
2024-03-07 22:28:16 +0100

    Adding credits

--- src/states/ending.py
+++ src/states/ending.py
@@ -1,5 +1,7 @@
 import pygame
+from objects.text import Text
 import utils.assets
+import configs
 from interfaces.state import State
 from objects.background import Background
 
@@ -17,6 +19,18 @@ class Ending(State):
         self.sprites = pygame.sprite.LayeredUpdates()
         # Create Game objects
         self.sprites.add(Background())
+        self.created_text = Text(configs.SCREEN_WIDTH // 2 - 50, configs.SCREEN_HEIGHT // 6, "Created by", "retro-font.ttf", 20, pygame.color.Color("white"))
+        self.creator_text = Text(configs.SCREEN_WIDTH // 2 - 40, configs.SCREEN_HEIGHT // 3.8, "jude erdrick", "score-font.ttf", 10, pygame.color.Color("white"))
+        self.graphics_text = Text(configs.SCREEN_WIDTH // 2 - 40, configs.SCREEN_HEIGHT // 2.2, "Graphics", "retro-font.ttf", 20, pygame.color.Color("white"))
+        self.graphic_text = Text(configs.SCREEN_WIDTH // 2 - 25, configs.SCREEN_HEIGHT // 1.8, "ansimuz", "score-font.ttf", 10, pygame.color.Color("white"))
+        self.musics_text = Text(configs.SCREEN_WIDTH // 2 - 75, configs.SCREEN_HEIGHT // 1.4, "Musics & sounds", "retro-font.ttf", 20, pygame.color.Color("white"))
+        self.music_text = Text(configs.SCREEN_WIDTH // 2 - 35, configs.SCREEN_HEIGHT // 1.22, "juhani junkala", "score-font.ttf", 10, pygame.color.Color("white"))
+        self.sprites.add(self.created_text)
+        self.sprites.add(self.creator_text)
+        self.sprites.add(self.graphics_text)
+        self.sprites.add(self.graphic_text)
+        self.sprites.add(self.musics_text)
+        self.sprites.add(self.music_text)
         # Background music
         self.music = utils.assets.get_audio("ending.wav")
     
